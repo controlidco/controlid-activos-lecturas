0ddb968b2dfa9a70846ef9c8c0f2c65bf8e8af9a
Cristian Chaparro <cristianchaparroa@gmail.com>
2015-03-14 09:58:39 -0500

    Release

---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/Development/repositorios/controlid-activos-lectura/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/Development/repositorios/controlid-activos-lectura/platforms/android/ant-build/CordovaApp.ap_ \
+/Development/repositorios/controlid-activos-lectura/platforms/android/ant-build/classes.dex \
+/Development/repositorios/controlid-activos-lectura/platforms/android/CordovaLib/ant-build/classes.jar \
